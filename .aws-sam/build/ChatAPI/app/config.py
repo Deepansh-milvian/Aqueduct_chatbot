0fd63608ae487b70ae2c62e8351bd4c8fd473108
Deepansh Singh <deepanshsingh@Deepanshs-MacBook-Pro.local>
2024-12-18 19:02:03 +0530

    removed aws-sam from git tracking

--- .aws-sam/build/ChatAPI/app/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-# Access environment variables
-DATABASE_NAME = os.environ.get("DATABASE_NAME")
-OUTPUT_BUCKET = os.environ.get("OUTPUT_BUCKET")
-REGION = os.environ.get("REGION")
-AGENT_ID = os.environ.get("AGENT_ID")
--- .aws-sam/build/ChatAPI/tests/test_llm.py
+++
@@ -1,1 +0,0 @@
-# Add test cases for llm_integration.py here
